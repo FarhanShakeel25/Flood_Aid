97e751dc79ffaac8c6c5073e14a6447d6a126678
Mustaqeem <mustaqeem.uet@gmail.com>
2025-12-19 16:05:33 +0500

    Home Page fully ready

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -42,8 +42,8 @@ export function Footer() {
                 </a>
               </li>
               <li>
-                <a href="#volunteer" className="footer-link">
-                  Volunteer
+                <a href="3contact" className="footer-link">
+                  Contact
                 </a>
               </li>
             </ul>
@@ -61,7 +61,7 @@ export function Footer() {
                 >
                   <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
                 </svg>
-                <span>Emergency: 911</span>
+                <span>Emergency: 1122</span>
               </li>
               <li className="contact-item">
                 <svg
